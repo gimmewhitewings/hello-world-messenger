2d612bf1d194421c69ee4f9af67e27baffbb5bd4
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-03-11 00:32:55 +0300

    Bottom menu done

--- app/src/main/java/com/example/helloworldmessenger/MainActivity.kt
+++ app/src/main/java/com/example/helloworldmessenger/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         navController = navHostFragment.navController
-
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
