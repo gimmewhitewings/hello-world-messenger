73a17f6c068ee2c4b8f2b32b7512fa7726021aa9
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-03-13 23:21:03 +0300

    sign up data validation

--- app/src/main/java/com/example/helloworldmessenger/RegisterFragment.kt
+++ app/src/main/java/com/example/helloworldmessenger/RegisterFragment.kt
@@ -1,59 +1,93 @@
 package com.example.helloworldmessenger
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
+import com.example.helloworldmessenger.databinding.FragmentRegisterBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RegisterFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RegisterFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentRegisterBinding
+    private var encodedImage: String = "s"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegisterFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegisterFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.signUpButton.setOnClickListener {
+            if (isValidDetails())
+                signUp()
+        }
+    }
+
+    private fun signUp() {
+
+    }
+
+    private fun isValidDetails(): Boolean {
+        // check for avatar
+        if (encodedImage == "") {
+            Toast.makeText(context, getString(R.string.choose_avatar), Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        // check for name
+        if (binding.inputName.editText?.text.toString() == "") {
+            binding.inputName.error = getString(R.string.name_error)
+            return false
+        }
+
+        // check for email
+        if (binding.inputEmail.editText?.text.toString() == "") {
+            binding.inputEmail.error = getString(R.string.email_error)
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(
+                binding.inputEmail.editText?.text.toString()
+            ).matches()
+        ) {
+            binding.inputEmail.error = getString(R.string.email_error)
+            return false
+        }
+
+        // check for password
+        if (binding.inputPassword.editText?.text.toString() == "") {
+            binding.inputPassword.error = getString(R.string.password_error)
+            return false
+        }
+
+        // check for correct password
+        if (binding.inputConfirmPassword.editText?.text.toString() == "") {
+            binding.inputConfirmPassword.error = getString(R.string.password_error)
+            return false
+        } else if (binding.inputConfirmPassword.editText?.text.toString() != binding.inputPassword.editText?.text.toString()) {
+            binding.inputConfirmPassword.error = getString(R.string.confirm_password_error)
+            return false
+        }
+
+        // reset all errors
+        binding.inputName.error = null
+        binding.inputEmail.error = null
+        binding.inputPassword.error = null
+        binding.inputConfirmPassword.error = null
+
+        binding.inputName.isErrorEnabled = false
+        binding.inputEmail.isErrorEnabled = false
+        binding.inputPassword.isErrorEnabled = false
+        binding.inputConfirmPassword.isErrorEnabled = false
+
+        return true
     }
 }
---
+++ app/src/main/java/com/example/helloworldmessenger/utils/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.helloworldmessenger.utils
+
+class Constants {
+}
