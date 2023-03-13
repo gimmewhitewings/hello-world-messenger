e084389342f0b980f679baf069fe8106babcaaaf
Stanislav Povedenok <stas.egor.povedenok@gmail.com>
2023-03-13 19:48:50 +0300

    firebase implemented

--- app/src/main/java/com/example/helloworldmessenger/LogInFragment.kt
+++ app/src/main/java/com/example/helloworldmessenger/LogInFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.helloworldmessenger.databinding.FragmentLogInBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * A simple [Fragment] subclass.
@@ -37,4 +39,5 @@ class LogInFragment : Fragment() {
             findNavController().navigate(R.id.action_logInFragment_to_registerFragment)
         }
     }
+
 }
